e08a7db04dac052d10a28aa7deacb6cab70d411a
Guvilla15 <117367794+Guvilla15@users.noreply.github.com>
2022-12-18 11:48:02 -0300

    Alterção 4

---
+++ scripts.js
@@ -0,0 +1,19 @@
+/*let string ="123"
+console.log(Number(string))
+let number = 14525
+console.log(String(number))*/
+
+/*let word = "Paralepipedo"
+console.log(word.length)
+let number = 1234
+console.log(String(number).length)*/
+
+/*let number = 1357357319836.6129671262
+console.log(number.toFixed(2).replace(".",","))*/
+
+/*let word = "Programar é muito bacana"
+console.log(word.toLowerCase())
+console.log(word.toUpperCase())*/
+
+
+
